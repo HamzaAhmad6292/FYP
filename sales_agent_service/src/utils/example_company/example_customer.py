3b74d9d33ab140392a013250e6fe06a99958cfd0
HamzaAhmad6292 <hamzaahmad6292@gmail.com>
2025-03-14 13:22:38 +0500

    conflict resolved

--- sales_agent_service/src/utils/example_company/example_customer.py
+++ sales_agent_service/src/utils/example_company/example_customer.py
@@ -2,4 +2,5 @@ example_customer = {
   "customer_name": "Zain",
   "customer_company": "Food business",
   "company_description": "A medium size company that offer frozen food to vendors on demand and manages functions like weddings."
+
 }
