d77039749974cedfdd54e3663acee95861b92a39
Muhammad Hamza Ahmad <139109293+HamzaAhmad6292@users.noreply.github.com>
2025-03-14 21:15:55 +0500

    Revert "Sales"

--- crm_integration_service/app/supabase_client.py
+++ crm_integration_service/app/supabase_client.py
@@ -6,8 +6,7 @@ import os
 load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SERVICE_ROLE")
-print("SUPABASE_URL",SUPABASE_URL)
-print("SUPABASE_KEY",SUPABASE_KEY)
+
 
 
 
